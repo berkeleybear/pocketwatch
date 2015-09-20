3fd640ff89e0e3b77f984e22be28f99c7de24886
berkeleybear <alicewu1992@gmail.com>
2015-09-19 18:28:05 -0700

    login page

--- src/controllers/loginCtrl.js
+++ src/controllers/loginCtrl.js
@@ -1,5 +1,9 @@
-app.controller('mainController', function($scope, pwConfig){
+app.controller('mainController', function($scope, pwConfig, authService){
   
   $scope.message = "";
-
+  $scope.login = function() {
+    userData = {userData: [$scope.userName, $scope.userEmail]}
+    console.log(userData)
+    //Users.push(userData);
+  }
 });
